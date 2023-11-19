73ce80552d5bdfe8363f52e8d4fe55619ef9f624
Hakam Nabhani <koomy.coding@gmail.com>
2023-11-19 13:57:13 +0400

    deleted:    src/assets/toronto.jpg 	deleted:    src/assets/venice.jpg 	modified:   src/data.js

--- src/data.js
+++ src/data.js
@@ -13,7 +13,7 @@ export default [
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "./src/assets/toronto.jpg"
+        imageUrl: "https://cms.finnair.com/resource/blob/704052/b5c6b95656c13bf393e5b2c5c87a7cd0/toronto-main-data.jpg"
     },
     {
         title: "Venice",
@@ -21,6 +21,6 @@ export default [
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "./src/assets/venice.jpg"
+        imageUrl: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2020/04/07/16/istock-491391396.jpg"
     },
 ]
